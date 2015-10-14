5efb241a85e04fd3bce1338539b42e35753b9b80
soic-os <rchavda@umail.iu.edu>
2015-10-13 22:49:46 -0400

    committing assignment 4 for rchavda and ukarani

--- bbb-xinu/system/future_set.c
+++ bbb-xinu/system/future_set.c
@@ -1,6 +1,8 @@
 #include <future.h>
 syscall future_set(future *f, int *value)
 {
+	//register struct procent *prptr;
+	//prptr = &proctab[currpid];
 	
 	if(f->state == FUTURE_VALID || f->state == FUTURE_EMPTY)		
 	{
@@ -8,8 +10,15 @@ syscall future_set(future *f, int *value)
 	}
 	else
 	{
+		
+		//prptr->prstate = PR_READY;
+		//insert(currpid, readylist, prptr->prprio);
+		
 		f->value = value;
 		f->state = FUTURE_VALID;
+		resched();
+
+		
 		ready(f->pid);
 		return OK;
 	}
